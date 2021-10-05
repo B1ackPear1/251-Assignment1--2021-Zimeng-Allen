17f1aee5a8a01ce8ca994263a7f2683025287fee
Allen Wang <451366033@qq.com>
2021-10-05 19:45:05 +0800

    note editor

--- src/main/java/TextEditor.java
+++ src/main/java/TextEditor.java
@@ -1,3 +1,6 @@
+//import com.sun.jdi.event.StepEvent;
+import org.apache.commons.io.FileUtils;
+
 import javax.swing.*;
 import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.*;
@@ -22,6 +25,7 @@ public class TextEditor extends JFrame {
     private JMenuItem exititem = new JMenuItem("Exit");
     private JMenuItem copyitem = new JMenuItem("Copy");
     private JMenuItem pasteitem = new JMenuItem("Paste");
+    private JMenuItem aboutitem = new JMenuItem("About");
     private JPopupMenu right = new JPopupMenu();
     private JFileChooser jfc = new JFileChooser();
     private JScrollPane scroll = new JScrollPane(jta);
@@ -40,6 +44,7 @@ public class TextEditor extends JFrame {
         textEditor.openlistening();
         textEditor.exitlistening();
         textEditor.savelistening();
+        textEditor.aboutlistening();
     }
 
     public void window() {
@@ -52,9 +57,10 @@ public class TextEditor extends JFrame {
         jmb.add(helpmenu);
         filemenu.add(newitem);
         filemenu.add(openitem);
+        filemenu.add(saveitem);
         searchmenu.add(searchitem);
-        managemenu.add(saveitem);
         managemenu.add(exititem);
+        helpmenu.add(aboutitem);
         frame.setJMenuBar(jmb);
         frame.setSize(500, 500);
 //        frame.getContentPane().setBackground(Color.orange);
@@ -66,7 +72,7 @@ public class TextEditor extends JFrame {
         frame.getContentPane().add(scroll);
     }
 
-    public void news(){
+    public void news() throws IOException {
         JOptionPane panel = new JOptionPane();
         panel.setBackground(Color.cyan);
         int n = JOptionPane.showConfirmDialog(frame, "Are you sure you want to create a new window?");
@@ -87,16 +93,57 @@ public class TextEditor extends JFrame {
             @Override
             public void mousePressed(MouseEvent e) {
                 super.mousePressed(e);
-                news();
+                try {
+                    news();
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
             }
         });
     }
 
-    public void save(){
-
+    public void save() throws IOException {
+        if (frame.getTitle().equals("Text Editor")){
+            saveanother();
+        }else {
+            String fullpath = frame.getTitle();
+            FileUtils.write(new File(fullpath),jta.getText(),"utf-8",false);
+        }
     }
 
     public void saveanother() {
+        JFileChooser jfc1 = new JFileChooser();
+//        jfc1.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+        String name;
+        try{
+//            jfc1.addChoosableFileFilter(new FileNameExtensionFilter("txt","文本文档"));
+            if (jfc1.showSaveDialog(frame)==JFileChooser.APPROVE_OPTION){
+                String path = jfc1.getCurrentDirectory().getAbsolutePath();
+//                        if (!dir.exists()){
+//                            dir.getParentFile().mkdirs();
+//                        }
+//                        dir.createNewFile();
+                File f = jfc1.getSelectedFile();
+                if(jfc1.getSelectedFile()!=null){
+//                    name = f.getName();
+                    name = f.getAbsolutePath() + ".txt";
+//                    name = jfc1.getSelectedFile().getName();
+                }else{
+                    name = "assignment";
+                }
+//                String fullpath = path + "\\" + name;
+//                File file = new File(name,fullpath);
+//                if (!file.exists()){
+//                    file.createNewFile();
+//                }
+                BufferedWriter bf = new BufferedWriter(new FileWriter(name));
+                bf.write(jta.getText());
+                bf.flush();
+                bf.close();
+            }
+        }catch (Exception ex){
+            JOptionPane.showMessageDialog(frame,ex.getMessage());
+        }
 
     }
 
@@ -104,13 +151,36 @@ public class TextEditor extends JFrame {
         saveitem.addMouseListener(new MouseAdapter() {
             @Override
             public void mousePressed(MouseEvent e) {
-                save();
+                try {
+                    save();
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
             }
         });
     }
 
     public void open(){
-
+        try {
+            if (jfc.showOpenDialog(frame) != JFileChooser.APPROVE_OPTION) {
+                return;
+            }
+            String FileName = jfc.getSelectedFile().getAbsolutePath();
+            String framename = jfc.getSelectedFile().getName();
+            BufferedReader bf = new BufferedReader(new FileReader(FileName));
+            String string = bf.readLine();
+            String content = "";
+            while (string != null) {
+                content += string;
+                content += "\n";
+                string = bf.readLine();
+            }
+            bf.close();
+            frame.setTitle(framename);
+            jta.setText(content);
+        } catch (Exception ex) {
+            JOptionPane.showMessageDialog(frame, ex.getMessage());
+        }
     }
 
     public void openlistening(){
@@ -121,8 +191,26 @@ public class TextEditor extends JFrame {
             }
         });
     }
-    public void searching(){
+    class searching extends JDialog {
+        //a new searching window
+        public searching() {
+//            setVisible(true);
+//            setBounds(100,100,250,100);
+//            Container container = getContentPane();
+//            container.setBackground(Color.cyan);
+//            JTextField jTextField = new JTextField();
+//            container.add(jTextField);
+//            container.add(new JLabel("Please input your searching word:"));
+            JOptionPane panel = new JOptionPane();
+            panel.setBackground(Color.cyan);
+            String inputV = JOptionPane.showInputDialog("Please input a word ");
+            int length = inputV.length();
+            int a = jta.getText().indexOf(inputV);
+            jta.requestFocus();
+            jta.setSelectionStart(a);
+            jta.setSelectionEnd(a + length);
 
+        }
     }
 
     public void searchlistening() {
@@ -130,7 +218,7 @@ public class TextEditor extends JFrame {
             @Override
             public void mousePressed(MouseEvent e) {
                 super.mousePressed(e);
-                searching();
+                new searching();
             }
         });
     }
@@ -142,6 +230,23 @@ public class TextEditor extends JFrame {
                 super.mouseReleased(e);
                 frame.dispose();
                 System.exit(0);
+            }
+        });
+    }
+
+    public void about(){
+        String author = "This editor is written by Zimeng Fu and Allen Wang";
+        JOptionPane aboutt = new JOptionPane();
+        aboutt.setBackground(Color.cyan);
+        aboutt.showMessageDialog(frame, author,"Author",1);
+    }
+
+    public void aboutlistening(){
+        aboutitem.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                super.mousePressed(e);
+                about();
             }
         });
     }
